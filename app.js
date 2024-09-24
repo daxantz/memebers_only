2c4075abfd86190cc31e148f5d7174ab85011647
daxantz <daxantz@gmail.com>
2024-09-23 20:40:49 -0400

    inital setup of server with required libraries

--- app.js
+++ app.js
@@ -1,46 +1,41 @@
-require("dotenv").config({ path: "../../.env" });
-const { Client } = require("pg");
+require("dotenv").config();
 
-console.log("DB_HOST:", process.env.DB_HOST);
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_NAME:", process.env.DB_NAME);
-console.log("DB_PASSWORD:", process.env.DB_PASSWORD);
-console.log("DB_PORT:", process.env.DB_PORT);
+const express = require("express");
+const path = require("path");
+const db = require("./src/db/queries.js");
+const app = express();
+const initializePassport = require("./src/passport-config.js");
+const session = require("express-session");
+const methodOverride = require("method-override");
 
-const SQL = `
-CREATE TABLE IF NOT EXISTS users  (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  first_name VARCHAR (255) NOT NULL,
-  last_name VARCHAR (255) NOT NULL,
-  email VARCHAR (255) NOT NULL,
-  password VARCHAR (255) NOT NULL, 
-  isMember boolean NOT NULL
+const signUpRouter = require("./src/routes/sign-up.js");
+const loginRouter = require("./src/routes/login.js");
+const indexRouter = require("./src/routes/index.js");
+const passport = require("passport");
+const flash = require("express-flash");
+
+initializePassport(passport);
+app.use(express.static(path.join(__dirname, "public")));
+app.use(methodOverride("_method"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src", "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(flash());
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
 );
 
-CREATE TABLE IF NOT EXISTS messages (
-  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-  title VARCHAR ( 255 ) NOT NULL,
-  timestamp date NOT NULL, 
-  text VARCHAR (255) NOT NULL,
-  user_id INTEGER NOT NULL REFERENCES users(id)
-);
+app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(signUpRouter);
+app.use(loginRouter);
+app.use(indexRouter);
 
-
-
-
-
-`;
-
-async function main() {
-  console.log("seeding...");
-  const client = new Client({
-    connectionString: `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
-  });
-  await client.connect();
-  await client.query(SQL);
-  await client.end();
-  console.log("done");
-}
-
-main();
+app.listen(3000, (req, res) => {
+  console.log("Server running on port 3000");
+});
