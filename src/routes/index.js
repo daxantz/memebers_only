3efd3f2e5c452d35426dbe00ddc86855950df2b6
daxantz <daxantz@gmail.com>
2024-09-24 19:06:31 -0400

    moved authentication function to seperate file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,10 @@
 const { Router } = require("express");
 const router = Router();
-
+const checkAuthenticated =
+  require("../validators/authFunctions").checkAuthenticated;
 router.get("/", checkAuthenticated, (req, res) => {
   console.log(req.user);
-  res.render("index", { name: req.user.first_name });
+  res.render("index", { name: req.user.first_name, id: req.user.id });
 });
-function checkAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
-    return next();
-  }
 
-  res.redirect("/login");
-}
 module.exports = router;
