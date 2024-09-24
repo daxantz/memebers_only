ab641e7081b97240895f01212b4c28f8119148a8
daxantz <daxantz@gmail.com>
2024-09-24 19:03:12 -0400

    removed the authentication function that wasnt being used

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -4,11 +4,4 @@ exports.loginFormGet = (req, res) => {
   res.render("login");
 };
 
-function checkNotAuthenticated(req, res, next) {
-  if (req.isAuthenticated) {
-    res.redirect("/");
-  }
-  next();
-}
-
 exports.loginPut = (req, res) => {};
