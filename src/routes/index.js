c45f096f84540c938359e4cb1f7b2ce52ecf6e84
daxantz <daxantz@gmail.com>
2024-09-26 21:09:59 -0400

    added route handler to render all messages stored in backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,22 @@
 const { Router } = require("express");
 const router = Router();
+const db = require("../db/queries");
 const checkAuthenticated =
   require("../validators/authFunctions").checkAuthenticated;
-router.get("/", checkAuthenticated, (req, res) => {
-  console.log(req.user);
-  res.render("index", { name: req.user.first_name, id: req.user.id });
+router.get("/", checkAuthenticated, async (req, res) => {
+  try {
+    console.log(req.user);
+    const messages = await db.getMemberMessages();
+
+    res.render("index", {
+      name: req.user.first_name,
+      id: req.user.id,
+      messages: messages,
+      user: req.user,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
