2811d3018812f2d93795c532ffd36e860fb4a129
daxantz <daxantz@gmail.com>
2024-09-26 21:10:39 -0400

    created query functions to add new messages and get all messages

--- src/db/queries.js
+++ src/db/queries.js
@@ -25,10 +25,28 @@ async function getUserByEmail(email) {
 async function updateUserMemberStatus(id) {
   await pool.query("UPDATE users SET isMember = true WHERE id = $1 ", [id]);
 }
+
+async function insertMessage(title, text, timestamp, id) {
+  await pool.query(
+    "INSERT INTO messages (title,text, timestamp,  user_id ) VALUES ($1, $2, $3, $4)",
+    [title, text, timestamp, id]
+  );
+}
+
+async function getMemberMessages() {
+  const messages = await pool.query(
+    "Select title, timestamp, text, email, isMember from messages inner join users on messages.user_id = users.id where users.isMember = true"
+  );
+  console.log(messages.rows);
+  return messages.rows;
+}
+
 module.exports = {
   insertUser,
   getUsers,
   getUserById,
   getUserByEmail,
   updateUserMemberStatus,
+  insertMessage,
+  getMemberMessages,
 };
