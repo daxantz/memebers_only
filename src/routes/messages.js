5c5b8045e6fded8288f1028c4203096249422a21
daxantz <daxantz@gmail.com>
2024-09-29 21:48:48 -0400

    added end point for deleting messages

--- src/routes/messages.js
+++ src/routes/messages.js
@@ -1,7 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 const messagesController = require("../controllers/messages");
+const auth = require("../validators/authFunctions");
 
 router.get("/create-message", messagesController.createMessageFormGet);
 router.post("/create-message", messagesController.createMessagePut);
+router.delete(
+  "/delete/:id",
+  auth.checkAuthenticated,
+  messagesController.deleteMessage
+);
 module.exports = router;
