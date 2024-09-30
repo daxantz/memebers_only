751ce7c21742b92772f19fefb5b5de9de80097d0
daxantz <daxantz@gmail.com>
2024-09-29 21:47:32 -0400

    formatted timestamp property for ui

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,11 +7,21 @@ router.get("/", checkAuthenticated, async (req, res) => {
   try {
     console.log(req.user);
     const messages = await db.getMemberMessages();
+    //removes '00:00:00 GMT-0400 (Eastern Daylight Time)' from the timestamp string
+    const messagesWithDateFormat = messages.map((msg) => {
+      const substring = "00:00:00 GMT-0400 (Eastern Daylight Time)";
+      const fullTimeStamp = msg.timestamp.toString();
+      const subStringIndex = fullTimeStamp.indexOf(substring);
+
+      const formattedStamp = fullTimeStamp.slice(0, subStringIndex);
+
+      return { ...msg, timestamp: formattedStamp };
+    });
 
     res.render("index", {
       name: req.user.first_name,
       id: req.user.id,
-      messages: messages,
+      messages: messagesWithDateFormat,
       user: req.user,
     });
   } catch (error) {
