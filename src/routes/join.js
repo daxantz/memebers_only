0048f9639d829801cc6d38a47e1467424e33e5ef
daxantz <daxantz@gmail.com>
2024-09-29 21:48:27 -0400

    removed id param from routes because session is tracking the user

--- src/routes/join.js
+++ src/routes/join.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 const auth = require("../validators/authFunctions");
 const router = Router();
 const joinContoller = require("../controllers/join");
-router.get("/join/:id", auth.checkAuthenticated, joinContoller.joinFormGet);
+router.get("/join", auth.checkAuthenticated, joinContoller.joinFormGet);
 router.patch(
-  "/join/:id",
+  "/join/submit",
   auth.checkAuthenticated,
   joinContoller.setMemberStatusPut
 );
